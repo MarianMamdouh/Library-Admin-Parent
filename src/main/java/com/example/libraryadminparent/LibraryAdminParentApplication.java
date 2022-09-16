f956fcadd2d7ee46e44d2bf9bdff8c575cec97c3
mariansami <marian.sami@jumia.com>
2022-09-16 16:14:59 +0200

    Initial commit

---
+++ src/main/java/com/example/libraryadminparent/LibraryAdminParentApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryadminparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAdminParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAdminParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraryadminparent/LibraryAdminParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryadminparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAdminParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
